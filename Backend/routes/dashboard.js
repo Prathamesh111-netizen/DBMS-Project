66ded89eb244f022e71818606b3c557a79cc6547
Prathamesh <2002pratham1109@gmail.com>
2021-12-15 23:27:51 +0530

    login updated

--- Backend/routes/dashboard.js
+++ Backend/routes/dashboard.js
@@ -4,7 +4,7 @@ import db from "../models/index.js";
 const router = express.Router();
 
 router.get('/dashboard', (req,res)=>{
-   
+   res.render("dashboard", {email : req.session.Email});
 })
 
 export default router;
--- Backend/routes/login.js
+++ Backend/routes/login.js
@@ -1,34 +1,63 @@
+import bodyParser from "body-parser";
 import express from "express";
+import session from "express-session";
 import db from "../models/index.js";
 
-const router = express.Router();
+// const router = express.Router();
 const user = db.user;
+const app = express();
 
-router.get('/login', (req, res) => {
-    res.render("login");
+app.set('trust proxy', 1); // trust first proxy
+app.use(session({
+  secret: 'DBMSproject',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}));
+
+var newLogin;
+
+app.get('/login', (req, res) => {
+    if("loggedIn" in req.session && req.session.loggedIn == undefined)
+        res.redirect("dashboard");
+    else
+         res.render("login");
 })
 
-router.post('/login', (req, res) => {
+app.post('/login',
+    bodyParser.urlencoded(),
+
+    (req, res, next) => {
     console.log(req.body)
-    // res.send("Reached")
-    var newLogin = {
+    newLogin = {
         Email : req.body.Email,
         Password : req.body.Password
-    }
+    };
+
     user.findOne({
         where: {
             Email: newLogin.Email,
             Password: newLogin.Password
         }
     }).then((User) => {
-        if(!User){
-            
+        if(User){
+            console.log("User exists")
+            next();
         }
         else{
-           
+           console.log("User not exists")
+           res.sendStatus(401)
         }
-    })
+    });
+},
 
-})
+    (req, res)=>{
+        req.session.loggedIn = true;
+        req.session.Email = newLogin.Email,
+        console.log(req.session)
+        res.redirect('/dashboard')
+    }
+    
+)
 
-export default router;
+export default app;
