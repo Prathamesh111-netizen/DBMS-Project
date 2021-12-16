2dc6ec97a83318e48b9b42f6627db22f651a0cbb
Prathamesh <2002pratham1109@gmail.com>
2021-12-16 15:26:37 +0530

    commited final song page frontend

--- Backend/server.js
+++ Backend/server.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
    
-app.get('/home', (req,res)=>{
+app.get('/', (req,res)=>{
     res.render("home") 
 })
 
