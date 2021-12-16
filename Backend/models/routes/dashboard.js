46e57759c1c41f7da199dd9d192ef32e027f1f8f
Prathamesh <2002pratham1109@gmail.com>
2021-12-16 15:10:11 +0530

    commit

--- Backend/models/routes/dashboard.js
+++ Backend/models/routes/dashboard.js
@@ -4,7 +4,8 @@ import db from "../models/index.js";
 const router = express.Router();
 
 router.get('/dashboard', (req,res)=>{
-   res.render("dashboard", {email : req.session.Email});
+   // res.render("dashboard", {email : req.session.Email});
+   console.log(email);
 })
 
 export default router;
