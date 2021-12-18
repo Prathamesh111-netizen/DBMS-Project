07b42ffd001c88e5bdd0b3edc29255b0007c55f9
Karan Shah <karandood28@gmail.com>
2021-12-18 23:48:21 +0530

    Login and register will now work with the styling

--- Backend/routes/login.js
+++ Backend/routes/login.js
@@ -3,7 +3,7 @@ import express from "express";
 import session from "express-session";
 import db from "../models/index.js";
 
-const router = express.Router();
+// const router = express.Router();
 const user = db.user;
 const app = express();
 
@@ -61,30 +61,30 @@ app.post('/login',
     
 )
 
-router.post('/register', (req, res) => {
-    const newUser = {
-    User_Name: req.body.User_Name,
-    Email: req.body.Email,
-    Password: req.body.Password
-}
-console.log(newUser)
-let msg;
-user.findOne({ where: { Email: newUser.Email } })
-    .then((User) => {
-        if (User) {
-            console.log("Email present in the database");
-            // alert
-        }
-        else {
-            console.log("Email not present in the database");
-            user.create({
-                User_Name: req.body.User_Name,
-                Email: req.body.Email,
-                Password: req.body.Password
-            });
-            res.render("login");
-        }
-    })
-});
+// router.post('/register', (req, res) => {
+//     const newUser = {
+//     User_Name: req.body.User_Name,
+//     Email: req.body.Email,
+//     Password: req.body.Password
+// }
+// console.log(newUser)
+// let msg;
+// user.findOne({ where: { Email: newUser.Email } })
+//     .then((User) => {
+//         if (User) {
+//             console.log("Email present in the database");
+//             // alert
+//         }
+//         else {
+//             console.log("Email not present in the database");
+//             user.create({
+//                 User_Name: req.body.User_Name,
+//                 Email: req.body.Email,
+//                 Password: req.body.Password
+//             });
+//             res.render("login");
+//         }
+//     })
+// });
 
 export default app;
